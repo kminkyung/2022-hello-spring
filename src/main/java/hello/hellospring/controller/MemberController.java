481bab54f3a8222d7c36f9a499e555b6cb7b82a3
Minkyung <mminkyung.k@gmail.com>
2022-05-08 16:46:57 +0900

    Register endpoint to create member

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -31,4 +31,14 @@ public class MemberController {
     return "members/createMemberForm";
   }
 
+  @PostMapping("/members/new")
+  public String create(MemberForm form) {
+    Member member = new Member();
+    member.setName(form.getName());
+
+    memberService.join(member);
+
+    return "redirect:/";
+  }
+
 }
