ecc8044da14852c127d6a0b27b4eacfcdc58da74
Minkyung <mminkyung.k@gmail.com>
2022-05-08 16:46:32 +0900

    Add create member form template and MemberForm class

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,17 +1,34 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class MemberController {
-
   private final MemberService memberService;
+
+  // DI 필드 주입 방식
+//  @Autowired private MemberService memberService;
+
+  // DI setter 주입 방식
+//  @Autowired
+//  public void setMemberService(MemberService memberService) {
+//    this.memberService = memberService;
+//  }
 
   @Autowired
   public MemberController(MemberService memberService) {
     this.memberService = memberService;
+  }
+
+  @GetMapping("/members/new")
+  public String createForm() {
+    return "members/createMemberForm";
   }
 
 }
