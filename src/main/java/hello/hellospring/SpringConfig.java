0c51df68d9801203083aafbc6094428547b29dcf
Minkyung <mminkyung.k@gmail.com>
2022-05-21 21:11:43 +0900

    CRUD with Spring Data JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,34 +1,37 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
-//  private DataSource dataSource;
-  private final EntityManager em;
+  //  private DataSource dataSource;
+  //  private final EntityManager em;
+  private final MemberRepository memberRepository;
 
   @Autowired
-  public SpringConfig(DataSource dataSource, EntityManager em) {
-    this.em = em;
+  public SpringConfig(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
   }
 
-  // 직접 스프링 빈을 등록하는 방식
-  //  @Bean
-  //  public MemberService memberService() {
-  //    return new MemberService(memberRepository());
+  //  @Autowired
+  //  public SpringConfig(DataSource dataSource, EntityManager em) {
+  //    this.em = em;
   //  }
-  //
-  @Bean
-  public MemberRepository memberRepository() {
+
+  // 직접 스프링 빈을 등록하는 방식
+    @Bean
+    public MemberService memberService() {
+      return new MemberService(memberRepository);
+    }
+
+    //  @Bean
+    //  public MemberRepository memberRepository() {
     //    return new MemoryMemberRepository();
     //    return new JdbcTemplateMemberRepository(dataSource);
-    return new JpaMemberRepository(em);
-  }
+    //    return new JpaMemberRepository(em);
+//  }
 }
